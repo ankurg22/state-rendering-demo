3aae8a9a9d0b3dfae7090b9043c9ba11f3e06209
Ankur Gupta <ankur.gupta@hapramp.com>
2021-01-03 23:03:12 +0530

    Pass - greeting fetch failed with an error.

--- app/src/main/java/dev/ankurg/statetest/GreetingView.kt
+++ app/src/main/java/dev/ankurg/statetest/GreetingView.kt
@@ -2,8 +2,15 @@ package dev.ankurg.statetest
 
 interface GreetingView {
     fun render(state: GreetingState) {
-        showProgress(true)
+        when(state.fetchStatus){
+            FetchStatus.Loading -> showProgress(true)
+            FetchStatus.Failure -> {
+                showProgress(false)
+                showError(state.error)
+            }
+        }
     }
 
     fun showProgress(show: Boolean)
+    fun showError(error: String?)
 }
--- app/src/test/java/dev/ankurg/statetest/SpyableGreetingView.kt
+++ app/src/test/java/dev/ankurg/statetest/SpyableGreetingView.kt
@@ -2,4 +2,5 @@ package dev.ankurg.statetest
 
 open class SpyableGreetingView : GreetingView {
     override fun showProgress(show: Boolean) {}
+    override fun showError(error: String?) {}
 }
