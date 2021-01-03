80e93810b537e2cecb60b9b6a318563594e8fe6d
Ankur Gupta <ankur.gupta@hapramp.com>
2021-01-03 23:37:01 +0530

    Implement GreetingView on the activity.

--- app/src/main/java/dev/ankurg/statetest/MainActivity.kt
+++ app/src/main/java/dev/ankurg/statetest/MainActivity.kt
@@ -2,10 +2,26 @@ package dev.ankurg.statetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), GreetingView {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun showProgress(show: Boolean) {
+        progressBar.visibility = if (show) View.VISIBLE else View.GONE
+    }
+
+    override fun showError(error: String?) {
+        errorTextView.visibility = View.VISIBLE
+        errorTextView.text = error
+    }
+
+    override fun showGreeting(greeting: String?) {
+        greetingTextView.visibility = View.VISIBLE
+        greetingTextView.text = greeting
     }
 }
