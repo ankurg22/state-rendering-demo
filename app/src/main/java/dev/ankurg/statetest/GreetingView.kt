e199f6c725dc5ae4670c04e115ea17516faf9927
Ankur Gupta <ankur.gupta@hapramp.com>
2021-01-03 23:21:26 +0530

    Pass - greeting fetched successfully and shown to user.

--- app/src/main/java/dev/ankurg/statetest/GreetingView.kt
+++ app/src/main/java/dev/ankurg/statetest/GreetingView.kt
@@ -2,15 +2,22 @@ package dev.ankurg.statetest
 
 interface GreetingView {
     fun render(state: GreetingState) {
-        when(state.fetchStatus){
-            FetchStatus.Loading -> showProgress(true)
+        when (state.fetchStatus) {
+            FetchStatus.Loading -> {
+                showProgress(true)
+            }
             FetchStatus.Failure -> {
                 showProgress(false)
                 showError(state.error)
+            }
+            FetchStatus.Success -> {
+                showProgress(false)
+                showGreeting(state.greeting)
             }
         }
     }
 
     fun showProgress(show: Boolean)
     fun showError(error: String?)
+    fun showGreeting(greeting: String?)
 }
--- app/src/test/java/dev/ankurg/statetest/SpyableGreetingView.kt
+++ app/src/test/java/dev/ankurg/statetest/SpyableGreetingView.kt
@@ -3,4 +3,5 @@ package dev.ankurg.statetest
 open class SpyableGreetingView : GreetingView {
     override fun showProgress(show: Boolean) {}
     override fun showError(error: String?) {}
+    override fun showGreeting(greeting: String?) {}
 }
